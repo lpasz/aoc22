2d0959c6926be74240c61a72cecf252a14269f8e
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-12-01 14:20:31 -0300

    map instead of reduce

--- src/day-01/calorie-counting.clj
+++ src/day-01/calorie-counting.clj
@@ -2,19 +2,16 @@
 
 (def inp (slurp "src/day-01/inp.txt"))
 
-(defn- elfs-calories [text]
-  (->> (s/split-lines text)
-       (reduce (fn [[curr-elf-cal elfs-cal] snack-cal]
-                 (if (= "" snack-cal)
-                   [0 (conj elfs-cal curr-elf-cal)]
-                   [(+ (read-string snack-cal) curr-elf-cal) elfs-cal]))
-               [0 []])
-       (second)
+(defn- to-int [s] (Integer/parseInt s))
+
+(defn- elf-total-calories [text]
+  (->> (s/split text #"\n\n")
+       (map #(->> (s/split-lines %) (map to-int) (apply +)))
        (sort >)))
 
-(defn max-elf-cal [text] (->> (elfs-calories text) (first)))
+(defn max-elf-cal [text] (->> (elf-total-calories text) (first)))
 
-(defn top-3-elf-cal [text] (->> (elfs-calories text) (take 3) (apply +)))
+(defn top-3-elf-cal [text] (->> (elf-total-calories text) (take 3) (apply +)))
 
 (max-elf-cal inp) ;; 69528
 (top-3-elf-cal inp) ;; 206152

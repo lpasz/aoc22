fdecf1a161ae83ba82d3639fc2e0618a4e7df7ee
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-12-01 17:11:09 -0300

    Improve Elixir Code

--- src/day-01/calories-counting.ex
+++ src/day-01/calories-counting.ex
@@ -1,33 +1,18 @@
 defmodule CaloriesCounting do
-  def inp, do: File.read!("./src/day-01/inp.txt")
+  @inp File.read!("./src/day-01/inp.txt")
 
-  defp elf_calories(text) do
-    text
-    |> String.split("\n")
-    |> Enum.chunk_by(&(&1 == ""))
-    |> Enum.reject(&(&1 == [""]))
-    |> Enum.map(fn elfs ->
-      elfs
-      |> Enum.map(&String.to_integer/1)
-      |> Enum.sum()
-    end)
-    |> Enum.sort(:desc)
-  end
-
-  def max_elf_cal() do
-    inp()
-    |> elf_calories()
-    |> List.first()
-  end
+  def max_elf_cal(), do: @inp |> elfs_calories() |> Enum.max()
 
   def top_3_elf_cal() do
-    inp()
-    |> elf_calories()
-    |> Enum.take(3)
-    |> Enum.sum()
+    @inp |> elfs_calories() |> Enum.sort(:desc) |> Enum.take(3) |> Enum.sum()
+  end
+
+  defp elfs_calories(text), do: text |> String.split("\n\n") |> Enum.map(&sum_elf_calories/1)
+
+  defp sum_elf_calories(elf) do
+    elf |> String.split("\n") |> Enum.map(&String.to_integer/1) |> Enum.sum()
   end
 end
 
-
-CaloriesCounting.max_elf_cal() # 69528
-CaloriesCounting.top_3_elf_cal() # 206152
+69528 = CaloriesCounting.max_elf_cal()
+206_152 = CaloriesCounting.top_3_elf_cal()

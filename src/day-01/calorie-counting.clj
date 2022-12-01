20fc1ee84c986677a1bdfc9cb7bb193858ee8329
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-12-01 13:05:24 -0300

    Day 01 - Clojure

--- src/day-01/calorie-counting.clj
+++ src/day-01/calorie-counting.clj
@@ -1,18 +1,8 @@
-(ns aoc22.day-01.calorie-counting
-  (:require [clojure.string :as s]))
+(ns aoc22.day-01.calorie-counting (:require [clojure.string :as s]))
 
 (def inp (slurp "src/day-01/inp.txt"))
 
-(defn max-elf-cal [text]
-  (->> (s/split-lines text)
-       (reduce (fn [[curr-elf-cal max-elf-cal] snack-cal]
-                 (if (= "" snack-cal)
-                   [0 (max max-elf-cal curr-elf-cal)]
-                   [(+ (read-string snack-cal) curr-elf-cal) max-elf-cal]))
-               [0 0])
-       (second)))
-
-(defn top-3-max-elf-cal [text]
+(defn- elfs-calories [text]
   (->> (s/split-lines text)
        (reduce (fn [[curr-elf-cal elfs-cal] snack-cal]
                  (if (= "" snack-cal)
@@ -20,11 +10,11 @@
                    [(+ (read-string snack-cal) curr-elf-cal) elfs-cal]))
                [0 []])
        (second)
-       (sort >)
-       (take 3)
-       (apply +)))
+       (sort >)))
 
-(max-elf-cal inp) ;;69528
-(top-3-max-elf-cal inp) ;; 206152
+(defn max-elf-cal [text] (->> (elfs-calories text) (first)))
 
-(time (top-3-max-elf-cal inp))
+(defn top-3-elf-cal [text] (->> (elfs-calories text) (take 3) (apply +)))
+
+(max-elf-cal inp) ;; 69528
+(top-3-elf-cal inp) ;; 206152

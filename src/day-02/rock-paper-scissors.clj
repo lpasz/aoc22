425911e1ed289898d4ee527b1b6b23144005b66f
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-12-02 18:44:34 -0300

    Day 02 - Clojure Clean Up

--- src/day-02/rock-paper-scissors.clj
+++ src/day-02/rock-paper-scissors.clj
@@ -1,41 +1,42 @@
-(ns aoc22.day-02.rock-paper-scissors (:require [clojure.string :as s]))
+(ns aoc22.day-02.rock-paper-scissors
+  (:use [clojure.set :only [map-invert]])
+  (:require [clojure.string :as s]))
 
 (def ex1-encrypt {\A :rock \B :paper \C :scissor \X :rock \Y :paper \Z :scissor})
 (def ex2-encrypt {\X :lose \Y :tie \Z :win})
 (def win {:paper :rock :rock :scissor :scissor :paper})
-(def win-of (clojure.set/map-invert win))
+(def lose (map-invert win))
 (def points {:rock 1 :paper 2 :scissor 3 :win 6 :lose 0 :tie 3})
 (def inp (slurp "src/day-02/inp.txt"))
 
-(defn game-result [adv you]
-  (cond (= adv you) :tie
-        (= adv (win you))  :win
-        (= (win adv) you) :lose))
+(defn game-result [you adv]
+  (cond (= (-> you win) adv)  :win
+        (= adv you) :tie
+        :else :lose))
 
-(defn get-points [[adv you]] (+ (points you) (points (game-result adv you))))
-
-(defn my-play [adv result]
-  (cond (= :tie result) adv
-        (= :win result) (win-of adv)
-        (= :lose result) (win adv)))
-
-(defn make-result-happen [[adv result]] (+ (points result) (points (my-play adv result))))
+(defn get-result-points [[adv you]] (+ (points you) (points (game-result you adv))))
 
 (defn ex1 [text]
   (->> (s/split-lines text)
-       (map (fn [line] (->> (seq line)
-                            ((juxt #(ex1-encrypt (first %))
-                                   #(ex1-encrypt (nth % 2)))))))
-       (map get-points)
-       (apply +)))
+       (reduce (fn [acc line] (->> (seq line)
+                                   ((juxt #(ex1-encrypt (first %))
+                                          #(ex1-encrypt (nth % 2))))
+                                   get-result-points
+                                   (+ acc))) 0)))
+
+(defn my-play [[adv result]]
+  (cond (= :tie result) [adv adv]
+        (= :win result) [adv (lose adv)]
+        (= :lose result) [adv (win adv)]))
 
 (defn ex2 [text]
   (->> (s/split-lines text)
-       (map (fn [line] (->> (seq line)
-                            ((juxt #(ex1-encrypt (first %))
-                                   #(ex2-encrypt (nth % 2)))))))
-       (map make-result-happen)
-       (apply +)))
+       (reduce (fn [acc line] (->> (seq line)
+                                   ((juxt #(ex1-encrypt (first %))
+                                          #(ex2-encrypt (nth % 2))))
+                                   my-play
+                                   get-result-points
+                                   (+ acc))) 0)))
 
 (ex1 inp) ;; 14531
 (ex2 inp) ;; 11258

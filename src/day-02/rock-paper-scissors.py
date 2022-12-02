7f084e66108879f6479b6627e5ad27ab7ad0aa18
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-12-02 19:45:52 -0300

    Day 02 - Python Improvements

--- src/day-02/rock-paper-scissors.py
+++ src/day-02/rock-paper-scissors.py
@@ -1,37 +1,44 @@
-inp = open('./src/day-02/inp.txt').read()
-ex1_decrypt = {'A': 'rock', 'B': 'paper', 'C': 'scissor',
+EX1_DECRYPT = {'A': 'rock', 'B': 'paper', 'C': 'scissor',
                'X': 'rock', 'Y': 'paper', 'Z': 'scissor'}
-ex2_decrypt = {'X': 'lose', 'Y': 'tie', 'Z': 'win'}
-win = {'rock': 'scissor', 'paper': 'rock', 'scissor': 'paper'}
-points = {'rock': 1, 'paper': 2, 'scissor': 3, 'win': 6, 'lose': 0, 'tie': 3}
-lines = inp.split("\n")
-exp1 = 0
-exp2 = 0
+EX2_DECRYPT = {'X': 'lose', 'Y': 'tie', 'Z': 'win'}
+WIN = {'rock': 'scissor', 'paper': 'rock', 'scissor': 'paper'}
+POINTS = {'rock': 1, 'paper': 2, 'scissor': 3, 'win': 6, 'lose': 0, 'tie': 3}
 
-for line in lines:
-    [adv, me] = line.split(' ')
-    adv = ex1_decrypt[adv]
-    me_1 = ex1_decrypt[me]
-    me_2 = ''
-    result_1 = ''
-    result_2 = ex2_decrypt[me]
 
-    if adv == win[me_1]:
-        result_1 = 'win'
-    elif adv == me_1:
-        result_1 = 'tie'
+def round_result(your_play, adv_play):
+    if adv_play == WIN[your_play]:
+        return 'win'
+    elif adv_play == your_play:
+        return 'tie'
     else:
-        result_1 = 'lose'
+        return 'lose'
 
+
+def play_to_achive_result(expected_result, adv_play):
     if result_2 == 'lose':
-        me_2 = win[adv]
+        return WIN[adv]
     elif result_2 == 'tie':
-        me_2 = adv
+        return adv
     else:
-        me_2 = win[win[adv]]
+        return WIN[WIN[adv]]
 
-    exp1 += points[me_1] + points[result_1]
-    exp2 += points[me_2] + points[result_2]
 
-print(exp1)
-print(exp2)
+if __name__ == '__main__':
+    inp = open('./src/day-02/inp.txt').read()
+    lines = inp.split("\n")
+    exp1 = 0
+    exp2 = 0
+
+    for line in lines:
+        [adv, me] = line.split(' ')
+        adv = EX1_DECRYPT[adv]
+        me_1 = EX1_DECRYPT[me]
+        result_1 = round_result(me_1, adv)
+        result_2 = EX2_DECRYPT[me]
+        me_2 = play_to_achive_result(result_2, adv)
+
+        exp1 += POINTS[me_1] + POINTS[result_1]
+        exp2 += POINTS[me_2] + POINTS[result_2]
+
+    print(exp1)  # 14531
+    print(exp2)  # 11258

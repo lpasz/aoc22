8917de1af2fd8c4249231ea2a18a85de2c161837
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-12-03 10:49:34 -0300

    Day 03 - Clojure Clean up

--- src/day-03/rucksack-reorganization.clj
+++ src/day-03/rucksack-reorganization.clj
@@ -2,39 +2,36 @@
   (:require [clojure.string :as s]
             [clojure.set :as set]))
 
-(def ex-inp (str "vJrwpWtwJgWrhcsFMMfFFhFp\n"
-                 "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n"
-                 "PmmdzqPrVvPwwTWBwg\n"
-                 "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n"
-                 "ttgJtRGJQctTZtZT\n"
-                 "CrZsJsPPZsGzwwsLwLmpwMDw"))
-
+(def ex-inp (slurp "src/day-03/ex-inp.txt"))
 (def inp (slurp "src/day-03/inp.txt"))
 
-(def points (into {} (mapcat (fn [i] [[(char (+ i (int \a))) (+ i 1)]
-                                      [(char (+ i (int \A))) (+ i 27)]])
-                             (range 0 26))))
+(def priorities (into {} (mapcat (fn [i] [[(char (+ i (int \a))) (+ i 1)]
+                                          [(char (+ i (int \A))) (+ i 27)]])
+                                 (range 0 26))))
+
+(defn split-in-half [text]
+  (let [size (count text)
+        middle (quot size 2)]
+    (split-at middle (seq text))))
+
+(defn priority-of-intersection [items]
+  (->> items
+       (map set)
+       (apply set/intersection)
+       (first)
+       (priorities)))
 
 (defn ex1 [text]
   (->> (s/split-lines text)
-       (reduce (fn [acc line]
-                 (->> (-> (count line) (quot 2) (split-at (seq line)))
-                      (map set)
-                      (reduce set/intersection)
-                      (first)
-                      (points)
-                      (+ acc))) 0)))
+       (reduce #(+ %1 (-> %2 split-in-half priority-of-intersection)) 0)))
 
 (defn ex2 [text]
   (->> (s/split-lines text)
        (partition 3)
-       (reduce (fn [acc elves-group]
-                 (let [[elf1 elf2 elf3] (map set elves-group)
-                       inter (set/intersection elf1 elf2 elf3)]
-                   (+ acc (points (first inter))))) 0)))
+       (reduce #(+ %1 (priority-of-intersection %2)) 0)))
 
 
-(ex1 ex-inp) ;; 157
-(ex1 inp) ;; 7980
-(ex2 ex-inp) ;; 70
-(ex2 inp) ;; 2881
+(ex1 ex-inp)  ;; 157
+(ex1 inp)     ;; 7980
+(ex2 ex-inp)  ;; 70
+(ex2 inp)     ;; 2881

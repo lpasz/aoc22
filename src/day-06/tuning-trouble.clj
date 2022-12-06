6054ae07301f5aae481817c7f77cb0d11f94a249
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-12-06 09:55:43 -0300

    Day 06 - Clojure 1 & 2

---
+++ src/day-06/tuning-trouble.clj
@@ -0,0 +1,29 @@
+(ns aoc22.day-06.tuning-trouble
+  (:require [clojure.string :as s]))
+
+(def inp (slurp "src/day-06/inp.txt"))
+
+(defn first-distinct-seq [n coll]
+  (->> (partition n 1 coll)
+       (keep-indexed (fn [idx itm] (if (= n (count (set itm))) 
+                                     idx)))
+       (first)
+       (+ n)))
+
+(defn ex1 [text] (first-distinct-seq 4 text))
+
+(defn ex2 [text] (first-distinct-seq 14 text))
+
+(ex1 "mjqjpqmgbljsphdztnvjfqwrcgsmlb") ;; 7
+(ex1 "bvwbjplbgvbhsrlpgdmjqwftvncz") ;; 5
+(ex1 "nppdvjthqldpwncqszvftbrmjlhg") ;; 6
+(ex1 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") ;; 10
+(ex1 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") ;; 11
+(ex1 inp) ;; 1538
+
+(ex2 "mjqjpqmgbljsphdztnvjfqwrcgsmlb") ;; 19
+(ex2 "bvwbjplbgvbhsrlpgdmjqwftvncz") ;; 23
+(ex2 "nppdvjthqldpwncqszvftbrmjlhg") ;; 23
+(ex2 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") ;; 29
+(ex2 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") ;; 26
+(ex2 inp) ;; 2315

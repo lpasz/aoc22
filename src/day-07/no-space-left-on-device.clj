f4397d7fd3aa1c415fe989ed906e18a911454b5a
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-12-07 13:43:13 -0300

    Revert "Day 07 - Clean up"
    This reverts commit 54a9013d8a391c6a691946fe07b2d5a7ccfdd2c7.

--- src/day-07/no-space-left-on-device.clj
+++ src/day-07/no-space-left-on-device.clj
@@ -4,22 +4,21 @@
 (def ex-inp (slurp "src/day-07/ex-inp.txt"))
 (def inp (slurp "src/day-07/inp.txt"))
 
-(defn parse-line [line]
+(defn parse [line]
   (cond (s/starts-with? line "cd") (s/replace line #"(cd.|\n)" "")
-        (s/starts-with? line "ls") (->> (s/split line #"[^0-9]")
-                                        (reduce #(if (not-empty %2)
-                                                   (+ %1 (Integer/parseInt %2))
-                                                   %1)
-                                                0))))
+        (s/starts-with? line "ls") (reduce #(if (not-empty %2)
+                                              (+ %1 (Integer/parseInt %2))
+                                              %1)
+                                           0
+                                           (s/split line #"[^0-9]"))))
 
-(defn parse-cmds [text]
+(defn cmds [text]
   (->> (s/split text #"\$ ")
        (keep not-empty)
-       (map parse-line)))
+       (map parse)))
 
-(defn increase-folders-size [fs cd cmd]
-  (loop [cd cd 
-         fs fs]
+(defn add-to-folders [fs cd cmd]
+  (loop [cd cd fs fs]
     (if (not-empty cd)
       (recur (drop-last cd)
              (update fs (s/join "/" cd) #(if (nil? %)
@@ -27,21 +26,18 @@
                                            (+ cmd %))))
       fs)))
 
-(defn return-one-folder [full-path] (into [] (drop-last full-path)))
-(defn enter-in-folder [full-path new-folder] (conj full-path new-folder))
-
-(defn directory [text]
-  (->> (parse-cmds text)
-       (reduce (fn [[full-path dir-structure] cmd]
+(defn fs [text]
+  (->> (cmds text)
+       (reduce (fn [[cd fs] cmd]
                  (cond
-                   (number? cmd) [full-path (increase-folders-size dir-structure full-path cmd)]
-                   (= ".." cmd)  [(return-one-folder full-path) dir-structure]
-                   (string? cmd) [(enter-in-folder full-path cmd) dir-structure]))
+                   (number? cmd) [cd (add-to-folders fs cd cmd)]
+                   (= ".." cmd)  [(into [] (drop-last cd)) fs]
+                   (string? cmd) [(conj cd cmd) fs]))
                [[] {}])
        (second)))
 
 (defn ex1 [text]
-  (->> (directory text)
+  (->> (fs text)
        (vals)
        (filter number?)
        (filter #(>= 100000 %))
@@ -50,20 +46,16 @@
 (def total-space 70000000)
 (def space-req 30000000)
 
-(defn smallest-file-size-to-delete [files-size min-required-size]
-  (->> files-size
-       (filter #(> % min-required-size))
-       (sort)
-       (first)))
-
-
 (defn ex2 [text]
-  (let [directory (directory text)
-        used-space (directory "/")
+  (let [fs (fs text)
+        used-space (fs "/")
         remaining-space (- total-space used-space)
         need-delete-at-least (- space-req remaining-space)]
-    (->> (vals directory)
-         (smallest-file-size-to-delete need-delete-at-least))))
+    (->> fs
+         (vals)
+         (filter #(> % need-delete-at-least))
+         (sort)
+         (first))))
 
 (ex1 ex-inp)
 (ex1 inp)

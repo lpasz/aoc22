423285aec9135e567ba8ba75c5ee14f40e65bd22
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-12-10 13:43:42 -0300

    Refactory Day 10

--- src/day-10/cathode-ray-tube.clj
+++ src/day-10/cathode-ray-tube.clj
@@ -2,8 +2,7 @@
   (:require [clojure.string :as s]))
 
 (def inp (slurp "src/day-10/inp.txt"))
-(def ex-inp-01 (slurp "src/day-10/ex-inp-01.txt"))
-(def ex-inp-02 (slurp "src/day-10/ex-inp-02.txt"))
+(def ex-inp (slurp "src/day-10/ex-inp.txt"))
 
 (defn to-instruction [line]
   (if (= "noop" line)
@@ -12,8 +11,6 @@
      (->> (s/split line #" ")
           (last)
           (Integer/parseInt))]))
-
-(to-instruction "addx 3")
 
 (defn parse-inp [text]
   (->> (s/split-lines text)
@@ -25,30 +22,22 @@
                    [1 1])
        (into (sorted-map))))
 
-(def power-cycles '(20 60 100 140 180 220))
+(def cycles [20 60 100 140 180 220])
 
 (defn ex1 [text]
-  (let [m (parse-inp text)]
-    (loop [pcs power-cycles
-           sum 0]
-      (let [pc (first pcs)]
-        (if-let [t (m pc)]
-          (recur (rest pcs)
-                 (+ sum (* pc t)))
-          sum)))))
+  (let [ci (parse-inp text)]
+    (reduce #(+ %1 (* %2 (ci %2))) 0 cycles)))
 
 (defn bit-on? [cycle reg]
   (#{reg (+ 1 reg) (+ 2 reg)} (rem cycle 40)))
 
 (defn ex2 [text]
-  (let [m (parse-inp text)]
-    (->> m
-         (map (fn [[cycle reg]]
-                (if (bit-on? cycle reg) \# \.)))
-         (partition 40)
-         (map #(apply str %)))))
+  (->> (parse-inp text)
+       (map (fn [[cycle reg]] (if (bit-on? cycle reg) \# \.)))
+       (partition 40)
+       (map #(apply str %))))
 
-(ex1 ex-inp-02)
-(ex1 inp)
-(ex2 ex-inp-02)
-(ex2 inp)
+(ex1 ex-inp) ;; 13140
+(ex1 inp)    ;; 13440
+(ex2 ex-inp) ;; ~nothing
+(ex2 inp)    ;; PBZGRAZA

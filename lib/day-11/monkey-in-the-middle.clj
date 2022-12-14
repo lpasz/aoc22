6b37f6c5d424bf61f171e7b902f69118e0b5fa0f
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-12-14 12:28:53 -0300

    Day 11 - Clojure Immutable and Fast

--- lib/day-11/monkey-in-the-middle.clj
+++ lib/day-11/monkey-in-the-middle.clj
@@ -1,4 +1,4 @@
-(ns aoc22.day-11.monkey-in-the-middle
+(ns day-11.monkey-in-the-middle
   (:require [clojure.string :as s]))
 
 (def inp (slurp "lib/day-11/inp.txt"))
@@ -6,14 +6,10 @@
 
 (defn to-int [i] (Integer/parseInt i))
 
-(defn parse-monkey-number [monkey-number]
-  (Integer/parseInt (s/replace monkey-number #"[^0-9]" "")))
-
 (defn parse-starting-items [starting-items]
   (->> (s/split (last (s/split starting-items #": ")) #", ")
        (map to-int)
-       (into [])
-       (new java.util.ArrayList)))
+       (into [])))
 
 (defn parse-operation [operation]
   (-> (str "(fn [old] ("
@@ -29,10 +25,10 @@
      :divisible-for divisible-for}))
 
 (defn parse-line [line]
-  (let [[monkey-number starting-items operation & test] (s/split-lines line)]
+  (let [[_ starting-items operation & test] (s/split-lines line)]
     (merge {:items (parse-starting-items starting-items)
             :operation (parse-operation operation)
-            :inspected (volatile! 0)}
+            :inspected 0}
            (parse-test test))))
 
 (defn parse-inp [text]
@@ -40,24 +36,36 @@
        (map parse-line)
        (into [])))
 
-(defn inspect-worry-throw [{:keys [items operation test inspected]} monkeys relief denominator]
-  (do (vswap! inspected + (count items))
-      (doseq [item items]
-        (let [worry-lvl (operation item)
-              worry-lvl (quot worry-lvl relief)
-              worry-lvl (rem worry-lvl denominator)
-              next-monkey-items (:items (monkeys (test worry-lvl)))]
-          (. next-monkey-items add worry-lvl)))
-      (. items clear)))
+(defn throw-item-to-other-monkeys [monkeys item monkey-num relief denominator]
+  (let [monkey (get monkeys monkey-num)
+        worry-lvl ((:operation monkey) item)
+        worry-lvl (quot worry-lvl relief)
+        worry-lvl (rem worry-lvl denominator)
+        trow-to ((:test monkey) worry-lvl)]
+    (update-in monkeys [trow-to :items] conj worry-lvl)))
+
+(defn throw-all-item-to-other-monkeys [monkeys items monkey-num relief denominator]
+  (->> items
+       (reduce #(throw-item-to-other-monkeys %1 %2 monkey-num relief denominator) monkeys)))
+
+(defn do-monkey-turn [monkeys monkey-num relief denominator]
+  (let [items (get-in monkeys [monkey-num :items])]
+    (-> monkeys
+        (throw-all-item-to-other-monkeys items monkey-num relief denominator)
+        (update-in [monkey-num :items] (constantly []))
+        (update-in [monkey-num :inspected] + (count items)))))
+
+(defn do-turn [monkeys _curr_turn relief denominator]
+  (->> (count monkeys)
+       (range)
+       (reduce #(do-monkey-turn %1 %2 relief denominator) monkeys)))
 
 (defn do-turns [text turns relief]
   (let [monkeys (parse-inp text)
         denominator (->> monkeys (map :divisible-for) (reduce *))]
-    (dotimes [_ turns]
-      (doseq [monkey monkeys]
-        (inspect-worry-throw monkey monkeys relief denominator)))
-    (->> monkeys
-         (map #(deref (:inspected %)))
+    (->> (range turns)
+         (reduce #(do-turn %1 %2 relief denominator) monkeys)
+         (map :inspected)
          (sort >)
          (take 2)
          (apply *))))
@@ -69,5 +77,3 @@
 (ex2 ex-inp) ;; 2713310158
 (ex1 inp)    ;; 55458
 (ex2 inp)    ;; 14508081294
-
-(inc (volatile! 0))

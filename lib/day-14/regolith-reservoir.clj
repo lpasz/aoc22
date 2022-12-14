dff3fe027b355e37bab15b5aaceb605b974974d6
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-12-14 10:18:54 -0300

    Clean up and renaming!

--- lib/day-14/regolith-reservoir.clj
+++ lib/day-14/regolith-reservoir.clj
@@ -16,8 +16,6 @@
       (partition 2 (interleave (repeat (first x-range)) y-range))
       (partition 2 (interleave  x-range (repeat (first y-range)))))))
 
-(rock-edge [498 6] [496 6])
-
 (defn add-rock-edge [acc [from to]]
   (->> (rock-edge from to)
        (reduce #(assoc %1 (vec %2) :rock) acc)))
@@ -31,8 +29,6 @@
        (partition 2 1)
        (reduce add-rock-edge acc)))
 
-(parse-line {} "498,4 -> 498,6 -> 496,6")
-
 (defn parse-inp [text]
   (->> (s/split-lines text)
        (reduce parse-line {})))
@@ -41,53 +37,48 @@
 (defn left-diagonal [[sx sy]] [(dec sx) (inc sy)])
 (defn right-diagonal [[sx sy]] [(inc sx) (inc sy)])
 
-
 (defn move-sand [acc sand-pos]
   (cond
     (nil? (acc (down sand-pos))) (down sand-pos)
     (nil? (acc (left-diagonal sand-pos))) (left-diagonal sand-pos)
     (nil? (acc (right-diagonal sand-pos))) (right-diagonal sand-pos)))
 
-(defn abyss-sand-fall-sim [acc sand-start free-fall-after]
+(defn sand-clogged-entry? [acc entry] (= :sand (acc entry)))
+(defn sand-stoped? [sand-pos] (nil? sand-pos))
+(defn hit-infinity-floor? [[_x y] infinity-floor] (and infinity-floor (= y infinity-floor)))
+(defn free-fall? [[_x y] free-fall] (and free-fall (> y free-fall)))
+
+(defn sand-fall-sim [acc start-sand-pos {:keys [free-fall infinity-floor]}]
   (loop [blockers acc
-         sand-pos sand-start]
-    (let [next-sand-pos (move-sand blockers sand-pos)]
+         curr-sand-pos start-sand-pos]
+    (let [next-sand-pos (move-sand blockers curr-sand-pos)]
       (cond
-        (nil? next-sand-pos) (assoc blockers sand-pos :sand)
-        (> (second next-sand-pos) free-fall-after) :stop
+        (sand-clogged-entry? acc start-sand-pos) :stop
+        (sand-stoped? next-sand-pos) (assoc blockers curr-sand-pos :sand)
+        (free-fall? next-sand-pos free-fall)  :stop
+        (hit-infinity-floor? next-sand-pos infinity-floor) (assoc blockers curr-sand-pos :sand)
         :else (recur blockers next-sand-pos)))))
 
-(defn floor-sand-fall-sim [acc sand-start infinity-floor]
-  (loop [blockers acc
-         sand-pos sand-start]
-    (let [next-sand-pos (move-sand blockers sand-pos)]
-      (cond
-        (= :sand (acc sand-start)) :stop
-        (nil? next-sand-pos) (assoc blockers sand-pos :sand)
-        (= (second next-sand-pos) infinity-floor) (assoc blockers sand-pos :sand)
-        :else (recur blockers next-sand-pos)))))
-
-(defn sand-max-capacity [sim quirk acc]
+(defn sand-max-capacity [acc quirk]
   (loop [acc acc]
-    (let [next-acc (sim acc [500 0] quirk)]
+    (let [next-acc (sand-fall-sim acc [500 0] quirk)]
       (if (= next-acc :stop)
         (count (filter #(= %1 :sand) (vals acc)))
         (recur next-acc)))))
 
 (defn ex1 [text]
   (let [rocks (parse-inp text)
-        free-fall-after (apply max (map second (keys rocks)))]
-    (sand-max-capacity abyss-sand-fall-sim free-fall-after rocks)))
-
+        free-fall (apply max (map second (keys rocks)))]
+    (sand-max-capacity rocks {:free-fall free-fall})))
 
 (defn ex2 [text]
   (let [rocks (parse-inp text)
-        infinity-floor-y (+ 2 (apply max (map second (keys rocks))))]
-    (sand-max-capacity floor-sand-fall-sim infinity-floor-y rocks)))
+        infinity-floor (+ 2 (apply max (map second (keys rocks))))]
+    (sand-max-capacity rocks {:infinity-floor infinity-floor})))
 
 (ex1 ex-inp) ;; 24
 (ex1 inp) ;; 1003
 
-(ex2 ex-inp) ;; 93
+(ex2 ex-inp) ;; 93 
 (ex2 inp) ;; 25771
 
